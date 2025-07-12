5273e2faf63d81fcf2609e0cebec72fb09ef4f35
mindface <in00und@gmail.com>
2025-07-12 16:31:04 +0900

    settings ci

--- backer/godotask/model/db.go
+++ backer/godotask/model/db.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "host=localhost user=dbgodotask password=dbgodotask dbname=dbgodotask port=5432 sslmode=disable"
+	dsn := "host=localhost user=dbgodotask password=dbgodotask dbname=test_db port=5432 sslmode=disable"
 	var err error
   DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
