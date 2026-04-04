cf07a17bab3c0bc74b76c9b1e9785a8d9d003f33
takanori <asdfghjkl@takanoris-Mac-mini.local>
2026-04-04 21:54:59 +0900

    settings remove code

--- backer/godotask/infrastructure/router/router.go
+++ backer/godotask/infrastructure/router/router.go
@@ -25,23 +25,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// シンプルなCORSミドルウェア（デバッグ用）
-func CORSMiddlewareSimple() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT, DELETE")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
@@ -53,7 +36,6 @@ func setupRouter() *gin.Engine {
 	// r.Use(middleware.RateLimitMiddleware())
 
 	r.Use(static.Serve("/usr/local/go/godotask/static", static.LocalFile("./images", true)))
-	// r.LoadHTMLGlob("view/*.html")
 
   bookRepo := &repository.BookRepositoryImpl{DB: model.DB}
 	bookService := &service.BookService{Repo: bookRepo}
